23e642b1e0f4b39a277c5ee69000805dd50c64da
ihorek7777 <ihor.verkhohliad@nure.ua>
2018-05-01 21:28:09 +0300

    try to response builded index.html file by stream (wip yet)

--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,17 @@
+const path = require('path')
+const fs = require('fs')
 const Router = require('koa-router')
 const usersRouter = require('./users')
 
 const router = new Router()
+
+router.get('/', async ctx => {
+	const file = fs.readFileSync(path.join(__dirname, '..', '..', 'build', 'index.html'))
+
+	// console.log(readStream)
+	// readStream.pipe(ctx.res)
+	// ctx.response = file
+})
 
 router.use(usersRouter.routes())
 router.use(usersRouter.allowedMethods())
